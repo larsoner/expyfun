bc55a40c983af8cb2fa3d70fe523019649ec0528
Daniel McCloy <dan.mccloy@gmail.com>
2016-07-15 12:03:56 -0700

    fix: fix bgcolor test; set trigger dummy mode in tests that use pyglet as AC

--- expyfun/tests/test_experiment_controller.py
+++ expyfun/tests/test_experiment_controller.py
@@ -280,7 +280,11 @@ def test_ec(ac=None, rd=None):
         assert_raises(KeyError, ec.identify_trial, ec_id='foo')  # need ttl_id
         assert_raises(TypeError, ec.identify_trial, ec_id='foo', ttl_id='bar')
         assert_raises(ValueError, ec.identify_trial, ec_id='foo', ttl_id=[2])
+        print('283: playing: {}, progress: {} !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+              '!!!!!'.format(ec._playing, ec._trial_progress))
         ec.identify_trial(ec_id='foo', ttl_id=[0, 1])
+        print('286: playing: {}, progress: {} !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+              '!!!!!'.format(ec._playing, ec._trial_progress))
         #
         # Second: start_stimuli
         #
@@ -360,8 +364,8 @@ def test_button_presses_and_window_size():
     warnings.simplefilter('ignore')  # esc as quit key
     with ExperimentController(*std_args, audio_controller='pyglet',
                               response_device='keyboard', window_size=None,
-                              output_dir=None, full_screen=False,
-                              participant='foo', session='01',
+                              output_dir=None, full_screen=False, session='01',
+                              participant='foo', trigger_controller='dummy',
                               force_quit='escape', version='dev') as ec:
         warnings.simplefilter('always')
         fake_button_press(ec, '1', 0.3)
@@ -411,21 +415,21 @@ def test_background_color():
     with ExperimentController(*std_args, participant='foo', session='01',
                               output_dir=None, version='dev') as ec:
         ec.set_background_color('red')
-        ec.screen_text('red', color='white')
         ss = ec.screenshot()[:, :, :3]
-        white_mask = (ss == [255] * 3)
-        assert_true(white_mask.any())
-        red_mask = (ss == [255, 0, 0])
-        assert_true(red_mask.any())
-        assert_true(np.logical_or(white_mask, red_mask).all())
+        red_mask = (ss == [255, 0, 0]).all(axis=-1)
+        assert_true(red_mask.all())
+        ec.set_background_color('white')
+        ss = ec.screenshot()[:, :, :3]
+        white_mask = (ss == [255] * 3).all(axis=-1)
+        assert_true(white_mask.all())
         ec.flip()
-
         ec.set_background_color('0.5')
         visual.Rectangle(ec, [0, 0, 1, 1], fill_color='black').draw()
         ss = ec.screenshot()[:, :, :3]
-        gray_mask = (ss == [127] * 3) | (ss == [128] * 3)
+        gray_mask = ((ss == [127] * 3).all(axis=-1) |
+                     (ss == [128] * 3).all(axis=-1))
         assert_true(gray_mask.any())
-        black_mask = (ss == [0] * 3)
+        black_mask = (ss == [0] * 3).all(axis=-1)
         assert_true(black_mask.any())
         assert_true(np.logical_or(gray_mask, black_mask).all())
 
